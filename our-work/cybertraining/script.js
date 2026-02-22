5802b052d5391cd47ff24d823188d44295431272
Ayan khan <mdayankhan166@gmail.com>
2026-02-22 22:38:55 +0530

    All File attached

---
+++ our-work/cybertraining/script.js
@@ -0,0 +1,13 @@
+
+let ourworkbtn=document.getElementById("ourworkbtn")
+let menubar=document.getElementById("workmenu")
+
+ourworkbtn.addEventListener("click", function(e){
+    e.preventDefault();
+    menubar.style.display=menubar.style.display==="block"?none:"block"
+
+})
+document.addEventListener("click",function(e){
+    if(!ourworkbtn.contains(e.target)&&!menubar.contains(e.target))
+        menubar.style.display="none"
+})
